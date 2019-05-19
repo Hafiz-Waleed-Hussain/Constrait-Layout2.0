6d9257d2d244efa80f6aa1eea6637311f89cd835
Jenkins <Jenkins@localhost>
2019-05-20 07:01:41 +0800

    Barrier API - First Scenario complete

---
+++ app/src/main/java/com/example/constraintlayout20/barrier/BarrierScenarioOne.kt
@@ -0,0 +1,13 @@
+package com.example.constraintlayout20.barrier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.constraintlayout20.R
+
+class BarrierScenarioOne : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barrier_scenario_one)
+    }
+}
