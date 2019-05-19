8b132cdbbd40e670db17284d5b0ace259620c1be
Jenkins <Jenkins@localhost>
2019-05-20 00:29:15 +0800

    Constraint Layout 2.0

---
+++ app/src/main/java/com/example/constraintlayout20/barrier/BarrierActivity.kt
@@ -0,0 +1,12 @@
+package com.example.constraintlayout20.barrier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BarrierActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barrier)
+    }
+}
