dff423275aa32255a7fe1c08e337ddec3878ba2c
Jenkins <Jenkins@localhost>
2019-05-20 08:11:28 +0800

    Barrier API - Second  Scenario complete

---
+++ app/src/main/java/com/example/constraintlayout20/barrier/BarrierScenarioTwo.kt
@@ -0,0 +1,13 @@
+package com.example.constraintlayout20.barrier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.constraintlayout20.R
+
+class BarrierScenarioTwo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barrier_scenario_two)
+    }
+}
